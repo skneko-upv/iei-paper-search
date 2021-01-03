426713bf6e17e8e7af758efa89b450ef2f895c6d
SaltyKawaiiNeko <software@skneko.moe>
2021-01-03 17:04:41 +0100

    Progress in Bibtex data extraction

---
+++ DataExtractors/Bibtex/BibtexJsonConverter.cs
@@ -0,0 +1,20 @@
+﻿using BibTeXLibrary;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+namespace IEIPaperSearch.DataExtractors.BDLP
+{
+    public class BibtexJsonConverter
+    {
+        public string BibtexToJson(string bibtex)
+        {
+            var parser = new BibParser(new StringReader(bibtex));
+            var entries = parser.GetAllResult();
+            return JsonConvert.SerializeObject(entries);
+        }
+
+        public string BibtexToJson(IEnumerable<string> bibtex) =>
+            BibtexToJson(string.Join("\r\n\r\n", bibtex));
+    }
+}
