47c7d441f3e4c3b881b5a56d715cfb287256f7cb
SaltyKawaiiNeko <software@skneko.moe>
2021-01-04 14:12:34 +0100

    Begin load form

--- Pages/Load.cshtml.cs
+++ Pages/Load.cshtml.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using IEIPaperSearch.Services.DataLoaders;
@@ -11,13 +12,53 @@ namespace IEIPaperSearch.Pages
     public class LoadModel : PageModel
     {
         readonly IDataLoaderService loaderService;
+
         public LoadModel(IDataLoaderService loaderService)
         {
             this.loaderService = loaderService;
         }
     
-        public void OnGet()
+        public IActionResult OnGet()
         {
+            return Page();
+        }
+
+        [BindProperty]
+        public bool LoadFromDblp { get; set; }
+        [BindProperty]
+        public bool LoadFromIeeeXplore { get; set; }
+        [BindProperty]
+        public bool LoadFromGoogleScholar { get; set; }
+
+        [BindProperty]
+        [Range(1000,3000)]
+        public uint StartingYear { get; set; }
+        [BindProperty]
+        [Range(1000,3000)]
+        [GreaterThanOrEqualTo("StartingYear", ErrorMessage = "El año de final debe ser mayor o igual que el año de inicio.")]
+        public uint EndYear { get; set; }
+
+        public async Task<IActionResult> OnPostSubmit()
+        {
+            Console.WriteLine("Form sent");
+
+            if (!LoadFromDblp && !LoadFromIeeeXplore && !LoadFromGoogleScholar)
+            {
+                ModelState.AddModelError("", "Selecciona al menos una fuente.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            return Page();
+
+            loaderService.LoadFromIeeeXplore();
+
+            TempData["test"] = 3;
+
+            return RedirectToPage("/LoadResults");
         }
     }
 }
---
+++ Validators/GreaterThanOrEqualToAttribute.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace IEIPaperSearch.Pages
+{
+    internal class GreaterThanOrEqualToAttribute : ValidationAttribute
+    {
+        readonly string testedPropertyName;
+
+        public GreaterThanOrEqualToAttribute(string testedPropertyName)
+        {
+            this.testedPropertyName = testedPropertyName;
+        }
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            var propertyTestedInfo = validationContext.ObjectType.GetProperty(this.testedPropertyName);
+            if (testedPropertyName is null)
+            {
+                return new ValidationResult(string.Format("Unknown property {0}", this.testedPropertyName));
+            }
+
+            var propertyTestedValue = propertyTestedInfo!.GetValue(validationContext.ObjectInstance, null);
+            if (value is null || propertyTestedValue is null)
+            {
+                return ValidationResult.Success;
+            }
+
+            if (!IsComparableWithProperty(propertyTestedInfo, value.GetType()))
+            {
+                return new ValidationResult(string.Format("Property {0} is not comparable with this property.", this.testedPropertyName));
+            }
+
+            if ((dynamic)value >= (dynamic)propertyTestedValue)
+            {
+                return ValidationResult.Success;
+            }
+            else
+            {
+                return new ValidationResult(FormatErrorMessage(validationContext.DisplayName));
+            }
+        }
+
+        bool IsComparableWithProperty(PropertyInfo? propertyInfo, params Type[] typeArguments)
+        {
+            return typeof(IComparable<>).MakeGenericType(typeArguments).IsAssignableFrom(propertyInfo?.PropertyType);
+        }
+    }
+}
