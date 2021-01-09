4c65e81fbed55cb64e456de6731a9126b38613c7
SaltyKawaiiNeko <software@skneko.moe>
2021-01-09 18:32:06 +0100

    [WIP] Start search results GUI

---
+++ Pages/SearchResults.cshtml.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IEIPaperSearch.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IEIPaperSearch.Pages
+{
+    public class SearchResultsModel : PageModel
+    {
+        [BindProperty(SupportsGet = true)]
+        public ICollection<Submission> Results { get; set; }
+
+        public void OnGet()
+        {
+            // Coger resultados -> Results
+            //  Aquí:
+            //TempData.Get<ICollection<Submission>>("SearchResults");
+            //  En Search:
+            //TempData.Put<ICollection<Submission>>("SearchResults", searchService.Search(...));
+        }
+    }
+}
